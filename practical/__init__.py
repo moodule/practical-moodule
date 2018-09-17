7f62813cf90056e206743ca2415e4cd8d8760081
David Mougeolle <david.mougeolle@moodule.net>
2018-09-17 09:04:34 +0200

    expose all the functions on import

--- practical/__init__.py
+++ practical/__init__.py
@@ -1,7 +1,61 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for Practical Moodule."""
+"""
+================
+Internal toolbox
+================
+
+Documentation is available in the docstrings and
+online at __
+"""
+
+from __future__ import division, print_function, absolute_import
+
+from practical.arrays import (
+    convert_dict_to_array,
+    reshapes,
+    reshape_into_matrix,
+    reshape_into_vector)
+from practical.memory import (
+    memoize)
+from practical.types import (
+    typecheck,
+    anything,
+    one_of,
+    iterable,
+    numeric,
+    finite,
+    symbolic,
+    bounds,
+    specifications)
+from practical.units import (
+    convert_radian_to_degree,
+    convert_degree_to_radian)
 
 __author__ = """David Mougeolle"""
 __email__ = 'david.mougeolle@moodule.net'
 __version__ = '0.0.0'
+
+__all__ = [
+    'convert_dict_to_array',
+    'reshapes',
+    'reshape_into_matrix',
+    'reshape_into_vector']
+
+__all__ += [
+    'memoize']
+
+__all__ += [
+    'typecheck',
+    'anything',
+    'one_of',
+    'iterable',
+    'numeric',
+    'finite',
+    'symbolic',
+    'bounds',
+    'specifications']
+
+__all__ += [
+    'convert_radian_to_degree',
+    'convert_degree_to_radian']

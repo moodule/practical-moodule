fb79d7a6f1f909390d865a393ded8ebe05a58cb4
David Mougeolle <david.mougeolle@moodule.net>
2018-10-05 10:54:28 +0200

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ test_requirements = [
 
 setup(
     name='practical',
-    version='0.0.1',
+    version='0.1.0',
     description="Personal toolbox.",
     long_description=readme + '\n\n' + history,
     author="David Mougeolle",

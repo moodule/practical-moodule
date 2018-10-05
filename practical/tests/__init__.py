6b8bdfd99b676b56dc1d2088a3b68bc1db74bb36
David Mougeolle <david.mougeolle@moodule.net>
2018-10-05 10:54:37 +0200

    Bump version: 0.1.0 → 0.2.0

--- practical/tests/__init__.py
+++ practical/tests/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """David Mougeolle"""
 __email__ = 'david.mougeolle@moodule.net'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ test_requirements = [
 
 setup(
     name='practical',
-    version='0.1.0',
+    version='0.2.0',
     description="Personal toolbox.",
     long_description=readme + '\n\n' + history,
     author="David Mougeolle",

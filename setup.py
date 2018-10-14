824cef95c492bc86b29647426d4303939e7333d3
David Mougeolle <david.mougeolle@moodule.net>
2018-10-14 13:19:32 +0200

    new type predicates : trace_data & exactly

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ test_requirements = [
 
 setup(
     name='practical',
-    version='0.4.1',
+    version='0.4.2',
     description="Personal toolbox.",
     long_description=readme + '\n\n' + history,
     author="David Mougeolle",

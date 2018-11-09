92089290d3b2a5782f8dcca9c45c56308088aa5a
David Mougeolle <david.mougeolle@moodule.net>
2018-11-09 16:30:49 +0100

    made the decorator names uniform: infinitive verbs

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ test_requirements = [
 
 setup(
     name='practical',
-    version='0.4.6',
+    version='0.4.7',
     description="Personal toolbox.",
     long_description=readme + '\n\n' + history,
     author="David Mougeolle",
